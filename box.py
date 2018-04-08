5762941215c64dfacac1b4061f0a2c9a6b8fc7c6
Ondřej Fuchs <o.fuchs@seznam.cz>
2018-04-08 12:14:50 +0200

    shut

--- box.py
+++ box.py
@@ -6,26 +6,28 @@
 import RPi.GPIO as GPIO
 import os, subprocess, time
 
+# Deff which pit will be controlled
+buttonPin = 37
 # The numbers printed on the board
 GPIO.setmode(GPIO.BOARD)
 # Use the pull up i.e. expect output to be zero. When it goes to 1, GPIO is set.
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def checkFunkc(interval, buttonPin):
+def checkFunkc(interval):
+  global buttonPin
   print "TODO"
   
 
 def main(args):
-  # Deff which pit will be controlled
-  buttonPin = 37
   # Time period to check
   interval = 10
   # Function to detect manipulation
-  checkFunkc(interval, buttonPin)
+  checkFunkc(interval)
 
   return 0
 
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
+
 

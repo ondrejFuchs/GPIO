166a711214d604cb71ae3544a71b34e51d571236
Ondřej Fuchs <o.fuchs@seznam.cz>
2018-04-08 12:06:42 +0200

    shut

--- shutdown.py
+++ shutdown.py
@@ -13,21 +13,21 @@ GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def checkFunkc(interval, buttonPin):
   while True:
-  if (GPIO.input(buttonPin)):
-    # GPIO is 0
-    # print("GPIO is 0 ")
-    print("Time is %s " % (time.ctime()))
-    time.sleep(pause)
-  else:
-    # GPIO is 1. We are here because the sensors triggered the battery being low.
-    print("Shutdown initiated at %s " % (time.ctime()))
-    subprocess.call("shutdown -h 2 &", shell=True)
-    time.sleep(2)
-    # Flush any stdout messages before exiting..
-    sys.stdout.flush()
-    # exit the while monitoring loop.
-    exit()
-  # Flush any buffers if ^C or interrupt is pressed.
+    if (GPIO.input(buttonPin)):
+      # GPIO is 0
+      # print("GPIO is 0 ")
+      print("Time is %s " % (time.ctime()))
+      time.sleep(pause)
+    else:
+      # GPIO is 1. We are here because the sensors triggered the battery being low.
+      print("Shutdown initiated at %s " % (time.ctime()))
+      subprocess.call("shutdown -h 2 &", shell=True)
+      time.sleep(2)
+      # Flush any stdout messages before exiting..
+      sys.stdout.flush()
+      # exit the while monitoring loop.
+      exit()
+    # Flush any buffers if ^C or interrupt is pressed.
   sys.stdout.flush()
   
 

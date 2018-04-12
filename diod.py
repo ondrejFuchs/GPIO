023bb4a2323f780694e288dd5d98914c798be933
Your Name <you@example.com>
2018-04-12 15:43:58 +0000

    diod

--- diod.py
+++ diod.py
@@ -7,7 +7,7 @@ import RPi.GPIO as GPIO
 import os, subprocess, time
 
 # Deff which pit will be controlled
-buttonPin = 37
+buttonPin = 36
 # The numbers printed on the board
 GPIO.setmode(GPIO.BOARD)
 # Use the pull up i.e. expect output to be zero. When it goes to 1, GPIO is set.
@@ -15,7 +15,17 @@ GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def checkFunkc(interval):
   global buttonPin
-  print "TODO"
+  while True:
+    if (GPIO.input(buttonPin)):
+      # GPIO is 0
+      # print("GPIO is 0 ")
+      print("GPIO is 0")
+      time.sleep(1)
+    else:
+      # GPIO is 1. We are here because the sensors triggered the battery being low.
+      print("GPIO is 1")
+      time.sleep(1)
+      # Flush any stdout messages before exiting..
   
 
 def main(args):

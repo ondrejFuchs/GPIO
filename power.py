714d43c93204c38f8b4964707b18fa3cdddad5dc
Ondřej Fuchs <o.fuchs@seznam.cz>
2018-04-18 10:32:50 +0200

    power.py

--- power.py
+++ power.py
@@ -4,35 +4,49 @@
 # Script to detect change power from adapter to battery
 #
 import RPi.GPIO as GPIO
-import os, subprocess, time
+import os, subprocess, time, logging
 
 # Deff which pit will be controlled
 buttonPin = 36
+# Interval to check
+interval = 1
 # The numbers printed on the board
 GPIO.setmode(GPIO.BOARD)
 # Use the pull up i.e. expect output to be zero. When it goes to 1, GPIO is set.
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# Set format for loggin
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y-%m-%d_%H-%M-%S',filename='/var/log/power.log',level=logging.DEBUG)
 
-def checkFunkc(interval):
+def checkFunkc():
   global buttonPin
+  global interval
+  stateOld = 1
+  stateNow = 1
   while True:
+    stateOld = stateNow 
     if (GPIO.input(buttonPin)):
       # GPIO is 0
-      # print("GPIO is 0 ")
-      print("GPIO is 0")
-      time.sleep(1)
+      stateNow = 0
+      if stateNow != stateOld:
+        logging.debug('Changing the power to battery')
+        print("GPIO is 0")
+      time.sleep(interval)
     else:
-      # GPIO is 1. We are here because the sensors triggered the battery being low.
-      print("GPIO is 1")
-      time.sleep(1)
-      # Flush any stdout messages before exiting..
+      # GPIO is 1
+      stateNow = 1
+      if stateNow != stateOld:
+        logging.debug('Changing the power to adapter')
+        print("GPIO is 1")
+      #logging.debug('This message should go to the log file')
+      time.sleep(interval)
+     
   
-
 def main(args):
-  # Time period to check
-  interval = 10
+  # Make log file
+  if not os.path.exists('/var/log/power.log'):
+    os.mknod('/var/log/power.log')
   # Function to detect manipulation
-  checkFunkc(interval)
+  checkFunkc()
 
   return 0
 

5426175582093c5455672f8f48c4bf521913cc24
Ondřej Fuchs <o.fuchs@seznam.cz>
2018-04-18 11:07:45 +0200

    diod.py

--- diod.py
+++ diod.py
@@ -4,35 +4,49 @@
 # Script to detect the opening of the box using a photodiode
 #
 import RPi.GPIO as GPIO
-import os, subprocess, time
+import os, subprocess, time, logging
 
 # Deff which pit will be controlled
 buttonPin = 36
+# Interval to check
+interval = 1
+# Threshold of positive check
+threshold = 5
 # The numbers printed on the board
 GPIO.setmode(GPIO.BOARD)
 # Use the pull up i.e. expect output to be zero. When it goes to 1, GPIO is set.
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Set format for loggin
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y-%m-%d_%H-%M-%S',filename='/var/log/power.log',level=logging.DEBUG)
 
-def checkFunkc(interval):
+def checkFunkc():
   global buttonPin
+  global interval
+  global threshold
+  counter = 0
   while True:
     if (GPIO.input(buttonPin)):
       # GPIO is 0
-      # print("GPIO is 0 ")
-      print("GPIO is 0")
-      time.sleep(1)
+      counter = 0
+      print("Box is closed")
+      time.sleep(interval)
     else:
-      # GPIO is 1. We are here because the sensors triggered the battery being low.
-      print("GPIO is 1")
-      time.sleep(1)
-      # Flush any stdout messages before exiting..
+      # GPIO is 1
+      counter += 1
+      print("Box is open")
+      if counter > threshold:
+            logging.debug('Box is open')
+            print("ALARM")
+      #logging.debug('This message should go to the log file')
+      time.sleep(interval)
   
 
 def main(args):
-  # Time period to check
-  interval = 10
+  # Make log file
+  if not os.path.exists('/var/log/diod.log'):
+    os.mknod('/var/log/diod.log')
   # Function to detect manipulation
-  checkFunkc(interval)
+  checkFunkc()
 
   return 0
 
